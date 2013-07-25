a8adfbd70aa7f0d155c1808c42aca7f95cf82168
aboutaaron <aaron.colby.williams@gmail.com>
2013-07-25 11:01:14 -0700

    ran main.js through jshint. I am choosing to ignore camelCase.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,11 +16,11 @@ Fuego = {
 	map: {
 		svg: '',
 		projection: '',
-		path: '',
+		path: ''
 	},
 
 	createSVG: function () {
-		var s = Fuego.settings
+		var s = Fuego.settings;
 		var m = Fuego.map;
 
 		// Fetch the SVG
@@ -58,10 +58,10 @@ Fuego = {
 					.data(california.features)
 				.enter().append('path')
 					.attr('id', function(d) {
-						return d.properties.name.toLowerCase().replace(/\s/g,'-')
+						return d.properties.name.toLowerCase().replace(/\s/g,'-');
 					})
 					.attr('class', 'county')
-					.attr('d', m.path)
+					.attr('d', m.path);
 		});
 
 		Fuego.ignite();
@@ -70,60 +70,49 @@ Fuego = {
 	ignite: function () {
 		// Fetch fire JSON from Heroku App
 		d3.json('http://calfire-api.herokuapp.com/counties/', function (error, json) {
-			if (error) return console.warn(error);
+			if (error) { return console.warn(error); }
 			data = json;
 			// Color the SVG
 			Fuego.paint(json);
-		});
-	},
-
-	_ignite: function () {
-		// Fetch fire JSON
-		d3.json('http://0.0.0.0:3000/counties/', function (error, json) {
-			if (error) return console.warn(error);
-			data = json;
-			// Color the SVG
-			Fuego.paint(json);
-			console.log("****************************************\nYOU ARE USING THE DEV COMMAND OF .ignite\n****************************************");
 		});
 	},
 
 	paint: function (objects) {
-	  d3.selectAll('.county')
-	    .attr('fill', function(d) {
-	    	var abbr = this;
-	      var fires;
+		d3.selectAll('.county')
+			.attr('fill', function () {
+				var abbr = this;
+				var fires;
 
-			  objects.forEach(function (object) {
-			  	if (object.county.slug == abbr.id) {
-			  		fires = object.county.fires.length;
-			  		//console.log(abbr);
-			  	}
-			  });
-			  return Fuego.getColor(fires);
-		});
+				objects.forEach(function (object) {
+					if (object.county.slug === abbr.id) {
+						fires = object.county.fires.length;
+					}
+				});
+				return Fuego.getColor(fires);
+		    }
+      );
 	},
 
 	getColor: function (d) {
-    return d > 12 ? '#800026' :
-           d > 10  ? '#BD0026' :
-           d > 8  ? '#E31A1C' :
-           d > 6  ? '#FC4E2A' :
-           d > 4   ? '#FD8D3C' :
-           d > 2   ? '#FEB24C' :
-           d > 1   ? '#FED976' :
-           d > 0   ? '#FFEDA0' :
-                      '#E2E2E2';
+		return d > 12 ? '#800026' :
+					 d > 10  ? '#BD0026' :
+					 d > 8  ? '#E31A1C' :
+					 d > 6  ? '#FC4E2A' :
+					 d > 4   ? '#FD8D3C' :
+					 d > 2   ? '#FEB24C' :
+					 d > 1   ? '#FED976' :
+					 d > 0   ? '#FFEDA0' :
+											'#E2E2E2';
 	},
 
 	_responsive: function () {
 		d3.select(window)
-				.on('resize', Fuego._responsive);
+			.on('resize', Fuego._responsive);
 		d3.select('g').attr('transform', 'scale('+$('.container').width()/900+')');
 		$('svg').height($('.container').width()*0.618);
 	}
-}
+};
 
-jQuery(document).ready(function($) {
+jQuery(document).ready(function () {
 	Fuego.build();
 });
